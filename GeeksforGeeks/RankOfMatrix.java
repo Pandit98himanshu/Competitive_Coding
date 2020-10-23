253031f2166e21ae9c321ac5203ae2721e959420
Himanshu Shekhar <pandit98himanshu@gmail.com>
2020-10-23 15:57:18 +0530

    Adding new

---
+++ GeeksforGeeks/RankOfMatrix.java
@@ -0,0 +1,198 @@
+/**
+ * https://practice.geeksforgeeks.org/problems/rank-of-matrix/0/?category[]=Arrays&category[]=Mathematical&category[]=Strings&category[]=Hash&problemType=full&difficulty[]=1&page=1&query=category[]Arrayscategory[]Mathematicalcategory[]Stringscategory[]HashproblemTypefulldifficulty[]1page1
+ * Author : Himanshu Shekhar
+ */
+
+import java.util.*;
+import java.io.*;
+import java.text.*;
+
+public class RankOfMatrix
+{
+	public static BufferedReader in;
+	public static PrintWriter out;
+	public static StringBuilder sol;
+	public static Utilities util;
+	public static DecimalFormat formatter;
+	public static String pattern = "0.0000";
+
+	public static void main(String[] args) throws IOException
+	{	
+		in = new BufferedReader(new InputStreamReader(System.in));
+		out = new PrintWriter(System.out);
+		sol = new StringBuilder();
+		util = new Utilities();
+		formatter = new DecimalFormat(pattern);
+
+		int t = Integer.parseInt(in.readLine());
+		while(t-- > 0)
+		{
+			long[][] m = new long[3][3];
+			for (int i = 0; i < 3; i++)
+			{
+				String[] inp = in.readLine().trim().split(" ");
+				for (int j = 0; j < 3; j++)
+					m[i][j] = Long.parseLong(inp[j]);
+			}
+			
+			// rank of a 3x3 matrix can be found by simply checking it's determinant.
+			// If determinant is zero then rank is 3 otherwise rank may be either 2 or 1.
+			long rank = 1;
+			for (int i = 0; i < 3; i++)
+			{
+				if (m[1][i] != 0 && m[0][i] % m[1][i] != 0)
+				{
+					rank = 2;
+				}
+			}
+			rank = determinant(m) != 0 ? 3 : rank;
+			System.out.println("Determinant = " + determinant(m));
+			sol.append(rank + "\n");
+		}
+		out.println(sol);
+		out.close();
+	}
+	public static long determinant(long[][] m)
+	{
+		return m[0][0]*(m[1][1]*m[2][2] - m[1][2]*m[2][1]) - m[0][1]*(m[1][0]*m[2][2] - m[1][2]*m[2][0]) + m[0][2]*(m[1][0]*m[2][1] - m[1][1]*m[2][0]);
+	}
+}
+
+class Utilities
+{
+	public static boolean[] prime;
+	public static long[] fact;
+
+	public static int gcd(int a, int b)
+	{
+		return b == 0 ? a : gcd(b, a%b);
+	}
+	public static long gcd(long a, long b)
+	{
+		return b == 0 ? a : gcd(b, a%b);
+	}
+	// Sort according to 1st column, if values of 1st column are equal then sort according to 2nd column
+	static final Comparator<double[]> nx2ArrayComparator = new Comparator<double[]>()
+	{
+		@Override
+		public int compare(double[] x, double[] y)
+		{
+			if (x[0] != y[0])
+				if (x[0] > y[0])
+					return 1;
+				else
+					return -1;
+			else
+				if (x[1] > y[1])
+					return 1;
+				else
+					return -1;
+		}
+	};
+	public static int power(int a, int b)
+	{
+		int res = 1;
+		while (b > 0)
+		{
+			if (b % 2 == 1)
+				res = res * a;
+			a = a * a;
+			b = b / 2;
+		}
+		return res;
+	}
+	public static long power(long a, long b, long mod)
+	{
+		long res = 1;
+		a = a % mod;
+		b = b % mod;
+		while (b > 0)
+		{
+			if (b % 2 == 1)
+				res = (res * a) % mod;
+			a = (a * a) % mod;
+			b = b / 2;
+		}
+		return res;
+	}
+	public static long modInv(long a, long mod)
+	{
+		return power(a, mod - 2, mod);
+	}
+	// finds index of >= key
+	public static int upper_bound_equals(long[] a, long key)
+	{
+		int low = 0, high = a.length - 1;
+		int mid;
+		while (low < high)
+		{
+			mid = low + (high - low)/2;
+			if (a[mid] < key)
+				low = mid + 1;
+			else
+				high = mid;
+		}
+		return low;
+	}
+	// finds index of > key
+	public static int upper_bound(long[] a, long key)
+	{
+		int low = 0, high = a.length - 1;
+		int mid;
+		while (low < high)
+		{
+			mid = low + (high - low)/2;
+			if (a[mid] <= key)
+				low = mid + 1;
+			else
+				high = mid;
+		}
+		return low;
+	}
+	// finds index of <= key
+	public static int lower_bound_equals(long[] a, long key)
+	{
+		int low = 0, high = a.length - 1;
+		int mid;
+		while (low < high)
+		{
+			mid = low + (high - low)/2;
+			if (a[mid] > key)
+				high = mid;
+			else
+				low = mid - 1;
+		}
+		return low;
+	}
+
+	// finds index of < key
+	public static int lower_bound(long[] a, long key)
+	{
+		int low = 0, high = a.length - 1;
+		int mid;
+		while (low < high)
+		{
+			mid = low + (high - low)/2;
+			if (a[mid] >= key)
+				high = mid;
+			else
+				low = mid - 1;
+		}
+		return low;
+	}
+	public static void sieve(int n)
+	{
+		prime = new boolean[n+1];
+		Arrays.fill(prime, true);
+
+		for (int i = 2; i*i <= n; i++)
+			if (prime[i] == true)
+				for (int j = i*i; j <= n; j += i)
+					prime[j] = false;
+	}
+	public static boolean isPrime(int n)
+	{
+		return prime[n];
+	}
+}
+
